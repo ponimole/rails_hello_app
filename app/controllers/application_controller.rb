3cea8fc45a7a5e8d46da734649ad09e89073cf85
ponimole <onimolep@yahoo.com>
2020-02-16 02:08:15 -0600

    added security patch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  def index 
+    render file: "#{Rails.root}/some/file", formats: [:html] 
+  end 
+
   def hola
     render html: "¡hola, mundo!"
   end
